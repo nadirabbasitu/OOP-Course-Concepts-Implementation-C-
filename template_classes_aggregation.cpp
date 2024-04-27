7cc7cf6967eeebd7d95a502c9e1ada32a19ce7ab
nadirabbasitu <91781236+nadirabbasitu@users.noreply.github.com>
2024-04-28 00:46:26 +0500

    Create template_classes_aggregation.cpp

---
+++ template_classes_aggregation.cpp
@@ -0,0 +1,48 @@
+//customer.h file
+#include<iostream>
+using namespace std;
+#include<string>
+template<class T>
+class Customer 
+{
+    private:
+        T name;
+    public:
+        Customer (){}
+        Customer(T name) {
+            this->name=name;
+            //this->address=address;
+        }
+        T getName(){
+            return this->name;
+
+        }
+};
+
+//order.h file
+template<class U>
+class Order 
+{
+    private:
+        Customer<U> *customer; // aggregation
+    public:
+        Order(){}
+        void placeOrder(Customer<U> *customer){
+            this->customer = customer;   
+        }
+        void displayOrderDetails(){
+        cout << "Customer: " << customer->getName() <<endl;
+        }
+};
+//main.cpp file
+
+int main() {
+    // Create customer
+    Customer<string> customer("Ateeq Ahmed");
+    Order<string> order;
+    order.placeOrder(&customer);
+    order.displayOrderDetails();
+    return 0;
+}
+
+
