b9a8a03bfd141433a4b697452d82a08a6258f027
nadirabbasitu <91781236+nadirabbasitu@users.noreply.github.com>
2024-04-28 00:44:39 +0500

    Update template_classes_composition.cpp

--- template_classes_composition.cpp
+++ template_classes_composition.cpp
@@ -1,4 +1,4 @@
-##custome.h file
+//customer.h file
 #include<iostream>
 using namespace std;
 #include<string>
@@ -17,5 +17,32 @@ class Customer
             return this->name;
 
         }
-
 };
+
+//order.h file
+template<class U>
+class Order 
+{
+    private:
+        Customer<U> customer; // composition
+    public:
+        Order(){}
+        void placeOrder(Customer<U> customer){
+            this->customer = customer;   
+        }
+        void displayOrderDetails(){
+        cout << "Customer: " << customer.getName() <<endl;
+        }
+};
+//main.cpp file
+
+int main() {
+    // Create customer
+    Customer<string> customer("Ateeq Ahmed");
+    Order<string> order;
+    order.placeOrder(customer);
+    order.displayOrderDetails();
+    return 0;
+}
+
+
